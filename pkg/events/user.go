ad3d4308194198ff0e6bbc93c73cbceaa158dcba
Sayan80bayev <sayan123serv@gmail.com>
2026-04-09 16:20:20 +0500

    feat: user events

--- pkg/events/user.go
+++ pkg/events/user.go
@@ -2,10 +2,24 @@ package events
 
 import "github.com/google/uuid"
 
-const EventTypeUserRegistered = "user.registered"
+const (
+	EventTypeUserRegistered = "user.registered"
+	EventTypeUserUpdated    = "user.updated"
+	EventTypeUserDeleted    = "user.deleted"
+)
 
 type UserRegistered struct {
 	UserID   uuid.UUID `json:"user_id"`
 	Username string    `json:"username"`
 	Email    string    `json:"email"`
+}
+
+type UserUpdated struct {
+	UserID    uuid.UUID `json:"user_id"`
+	Username  string    `json:"username"`
+	AvatarURL string    `json:"avatar_url"`
+}
+
+type UserDeleted struct {
+	UserID uuid.UUID `json:"user_id"`
 }
